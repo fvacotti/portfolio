d82acd879c1a14c77257b4fb50de1dcc51050109
fvacotti <fvacotti@outlook.com>
2023-02-10 01:52:03 -0300

    nuevo menu

--- js/main.js
+++ js/main.js
@@ -1,46 +1,43 @@
 // --------------- SELECTOR SECCIONES Y MENÚ HAMBURGUESA --------------- //
 
 // seteo menú hamburguesa
-const menu = document.querySelector(".nav-menu");
-const openMenuBtn = document.querySelector(".open-menu");
-const closeMenuBtn = document.querySelector(".close-menu");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-// aparezco/desaparezco menú
-function toggleMenu() {
-	menu.classList.toggle("menu_opened");
-}
+hamburger.addEventListener("click", () => {
+	hamburger.classList.toggle("active");
+	navMenu.classList.toggle("active");
+})
 
-openMenuBtn.addEventListener("click", toggleMenu);
-closeMenuBtn.addEventListener("click", toggleMenu);
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+	hamburger.classList.remove("active");
+	navMenu.classList.remove("active");
+}))
+
+// selector secciones
 
 // agarro los links que tengan "#" en ".nav-menu" (^= es "que comience con...")
 const menuLinks = document.querySelectorAll('.nav-menu a[href^="#"]');
 
-// selector de secciones
+// selector
 
 const observer = new IntersectionObserver((entries) => {
-		entries.forEach((entry) => {
-			const id = entry.target.getAttribute("id");
-			const menuLink = document.querySelector(`.nav-menu a[href="#${id}"]`);
+	entries.forEach((entry) => {
+		const id = entry.target.getAttribute("id");
+		const menuLink = document.querySelector(`.nav-menu a[href="#${id}"]`);
 
-			if (entry.isIntersecting) {
-				document.querySelector(".nav-menu a.selected").classList.remove("selected");
-				menuLink.classList.add("selected");
-			}
-		});
-	},
+		if (entry.isIntersecting) {
+			document.querySelector(".nav-menu a.selected").classList.remove("selected");
+			menuLink.classList.add("selected");
+		}
+	});
+},
 
 	// para que no se choquen elementos que estén muy juntos	
 	{ rootMargin: "-30% 0px -70% 0px" }
 );
 
-// hamburguesa
-
 menuLinks.forEach((menuLink) => {
-	menuLink.addEventListener("click", function () {
-		menu.classList.remove("menu_opened");
-	});
-
 	const hash = menuLink.getAttribute("href");
 	const target = document.querySelector(hash);
 	if (target) {
